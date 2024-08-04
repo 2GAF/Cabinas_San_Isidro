af870b43307216f3154d279703428ea5af78138e
DrexGuzman <d.jesusgc95@gmail.com>
2024-08-03 21:32:22 -0600

    Incorporar fotos al arreglo de useCabInfo.jspara ser mostradas en cada tarjeta y nuevas imagenes

--- src/buttons/Opcion.jsx
+++ src/buttons/Opcion.jsx
@@ -1,6 +1,6 @@
 export function Opcion({setter, text, isActive}){
 
-    const clasesBase = "text-[clamp(18px,_1.5vw,_1.875rem)] border-[3.5px] py-3 w-36 sm:w-48 rounded-2xl border-blue-1"
+    const clasesBase = "text-[clamp(18px,_1.5vw,_1.875rem)] border-[3.5px] py-3 w-36 sm:w-48 rounded-2xl border-blue-1 text-ovo"
 
     const clasesActivo = "text-white bg-blue-1"
     const clasesInactivo = "text-blue-1 bg-white"
--- src/hooks/useCabInfo.js
+++ src/hooks/useCabInfo.js
@@ -1,4 +1,19 @@
 import { useState, useEffect } from 'react';
+import c41 from "../assets/imgs/C4.1.jpg";
+import c42 from "../assets/imgs/C4.2.jpg";
+import c43 from "../assets/imgs/C4.3.jpg";
+import c61 from "../assets/imgs/C6.1.jpg";
+import c62 from "../assets/imgs/C6.2.jpg";
+import c63 from "../assets/imgs/C6.3.jpg";
+import c64 from "../assets/imgs/C6.4.jpg";
+import c65 from "../assets/imgs/C6.5.jpg";
+import c91 from "../assets/imgs/C9.1.jpg";
+import c92 from "../assets/imgs/C9.2.jpg";
+import c93 from "../assets/imgs/C9.3.jpg";
+import c94 from "../assets/imgs/C9.4.jpg";
+import c95 from "../assets/imgs/C9.5.jpg";
+import c96 from "../assets/imgs/C9.6.jpg";
+import c97 from "../assets/imgs/C9.7.jpg";
 
 // Información de las cabinas
 const cabinasInfo = {
@@ -6,56 +21,48 @@ const cabinasInfo = {
     titulo: "Cabina para 4 personas",
     precio: "₡60.000 IVA.i",
     descripcion: "Cabina equipada con utensilios básicos de cocina (platos, vasos, cubiertos, ollas, sarten, refrigeradora), cuenta con un aposento cocina-dormitorio con 1 cama matrimonial y 1 cama nido.",
-    servicios: "TV, Abanico",
-    image: "foto"
+    image: [c41, c42, c43],
   },
   '5-6': {
     titulo: "Cabina para 6 personas",
     precio: "₡73.000 IVA.i",
     descripcion: "Cabina equipada con utensilios básicos de cocina (platos, vasos, cubiertos, ollas, sarten, refrigeradora), cuenta con dos aposentos cocina y dormitorio con 1 cama matrimonial y 2 camas nido.",
-    servicios: "TV, Abanico",
-    image: "foto"
+    image: [c61, c62, c63, c64, c65],
   },
   '5-6*': {
     titulo: "Cabina para 6 personas",
     precio: "₡83.000 IVA.i",
     descripcion: "Cabina equipada con utensilios básicos de cocina (platos, vasos, cubiertos, ollas, sarten, refrigeradora), cuenta con dos aposentos cocina y dormitorio con 1 cama matrimonial y 2 camas nido.",
-    servicios: "TV, A/C",
-    image: "foto"
+    image: [c61, c62, c63, c64, c65],
   },
   '8-9': {
     titulo: "Cabina para 9 personas",
     precio: "₡110.000 IVA.i",
     descripcion: "Cabina equipada con utensilios básicos de cocina (platos, vasos, cubiertos, ollas, sarten, refrigeradora), cuenta con tres aposentos cocina y dos dormitorio cada uno con 1 cama matrimonial y 1 camas nido.",
-    servicios: "TV, Abanico",
-    image: "foto"
+    image: [c91, c92, c93, c94, c95, c96, c97],
   },
   '1': {
     titulo: "Habitación para 1 persona",
     precio: "₡25.000 IVA.i",
     descripcion: "Habitación sencilla con aire acondicionado y televisión, cuenta con una cama matrimonial y paños",
-    servicios: "A/C, TV",
     image: "foto"
   },
   '2': {
     titulo: "Habitación para 2 personas",
     precio: "₡36.000 IVA.i",
     descripcion: "Habitación sencilla con aire acondicionado y televisión, cuenta con una cama matrimonial y paños",
-    servicios: "A/C, TV",
     image: "foto"
   },
   '3': {
     titulo: "Habitación para 3 personas",
     precio: "₡42.000 IVA.i",
     descripcion: "Habitación sencilla con aire acondicionado y televisión, cuenta con una cama matrimonial, una cama individual y paños",
-    servicios: "A/C, TV",
     image: "foto"
   },
   'default': {
     titulo: "Habitación para 4 personas",
     precio: "₡47.000 IVA.i",
     descripcion: "Habitación sencilla con aire acondicionado y televisión, cuenta con dos camas matrimoniales y paños",
-    servicios: "A/C, TV",
     image: "foto"
   }
 };
