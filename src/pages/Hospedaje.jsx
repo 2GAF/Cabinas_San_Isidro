224e978d33833681a2169856bfb52a883e91064d
DrexGuzman <d.jesusgc95@gmail.com>
2024-08-11 20:15:08 -0600

    mejoras de sonarCloud

--- src/pages/Hospedaje.jsx
+++ src/pages/Hospedaje.jsx
@@ -1,7 +1,7 @@
 import { Header } from "../common/Header.jsx";
 import { Footer } from "../common/Footer.jsx";
 import { Opcion } from "../buttons/Opcion.jsx";
-import { useState, useEffect, useRef  } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Cantidad } from "../buttons/Cantidad.jsx";
 import fanBlue from "../assets/fanBlack.svg";
 import tvBlue from "../assets/tvBlack.svg";
@@ -13,7 +13,7 @@ import { useLocation } from "react-router-dom";
 
 export function Hospedaje() {
     const location = useLocation();
-    const [activeTab, setActiveTab] = useState(location.state||'Cabinas');
+    const [activeTab, setActiveTab] = useState(location.state || 'Cabinas');
     const [activeCap, setActiveCap] = useState('3-4');
     const { cabinaEscogida } = useCabInfo(activeCap);
 
@@ -73,26 +73,34 @@ export function Hospedaje() {
             <Opcion setter={setActiveTab} text='Cabinas' isActive={activeTab} />
         </div>
     );
-    
-    const renderButtonSection = (posicion) => (
-        <div className="flex justify-center py-5 md:py-14">
-            <button className="w-custom-img relative py-3" onClick={manejarClick} ref={tarjetaRef}>
-                <img className={`transition-transform duration-500 ease-in-out rounded-2xl ${tarjetaAlFrente ? 'z-0' : 'z-10'}`} src={imagenes[indiceActual]} alt="" />
-                <div className={`absolute top-1 lg:top-12 ${posicion} md:right-[-6rem] w-[11.25rem] h-[12.375rem] md:w-[20.375rem] md:h-[15.375rem] lg:w-[20rem] lg:h-[20rem] md:top-2 bg-blue-1 font-outfit text-white flex items-center justify-center shadow-black shadow-lg rounded-lg transition-transform duration-500 ease-in-out ${tarjetaAlFrente ? 'z-0 transform translate-x-0 ' : `z-[-10] transform ${posicion === 'right-[-2rem]' ? '-translate-x-10' : 'translate-x-10'}`}`}>
-                    <div className="px-[0.626rem] flex flex-col gap-y-[0.625rem]">
-                        <h1 className="text-3xl font-medium hidden md:block">{cabinaEscogida?.titulo}</h1>
-                        <h1 className="text-xl font-bold md:font-light">{cabinaEscogida?.precio}</h1>
-                        <p className="font-outfit text-base hidden lg:block">
-                            {cabinaEscogida?.descripcion}
-                        </p>
-                        <p className="text-center text-base font-light lg:hidden">{activeTab === 'Cabinas' ? 'Ideal para las familias pequeñas y grandes.' : 'Ideal para parejas y trabajadores.'}</p>
-                        <a href="https://wa.me/message/ZGYH7OW6HZAEN1"><div className="bg-white rounded-xl py-[0.313rem] flex justify-center text-blue-1 text-ovo text-base">Reservar</div></a>
+
+    const renderButtonSection = (posicion) => {
+        const positionClass = `${posicion} md:right-[-6rem]`;
+        const sizeClass = 'w-[11.25rem] h-[12.375rem] md:w-[20.375rem] md:h-[15.375rem] lg:w-[20rem] lg:h-[20rem]';
+        const topClass = `absolute top-1 lg:top-12 md:top-2`;
+        const transformClass = tarjetaAlFrente
+            ? 'z-0 transform translate-x-0'
+            : `z-[-10] transform ${posicion === 'right-[-2rem]' ? '-translate-x-10' : 'translate-x-10'}`;
+        return (
+            <div className="flex justify-center py-5 md:py-14">
+                <button className="w-custom-img relative py-3" onClick={manejarClick} ref={tarjetaRef}>
+                    <img className={`transition-transform duration-500 ease-in-out rounded-2xl ${tarjetaAlFrente ? 'z-0' : 'z-10'}`} src={imagenes[indiceActual]} alt="" />
+                    <div className={`${topClass} ${positionClass} ${sizeClass} bg-blue-1 font-outfit text-white flex items-center justify-center shadow-black shadow-lg rounded-lg transition-transform duration-500 ease-in-out ${transformClass}`}>
+                        <div className="px-[0.626rem] flex flex-col gap-y-[0.625rem]">
+                            <h1 className="text-3xl font-medium hidden md:block">{cabinaEscogida?.titulo}</h1>
+                            <h1 className="text-xl font-bold md:font-light">{cabinaEscogida?.precio}</h1>
+                            <p className="font-outfit text-base hidden lg:block">
+                                {cabinaEscogida?.descripcion}
+                            </p>
+                            <p className="text-center text-base font-light lg:hidden">{activeTab === 'Cabinas' ? 'Ideal para las familias pequeñas y grandes.' : 'Ideal para parejas y trabajadores.'}</p>
+                            <a href="https://wa.me/message/ZGYH7OW6HZAEN1"><div className="bg-white rounded-xl py-[0.313rem] flex justify-center text-blue-1 text-ovo text-base">Reservar</div></a>
+                        </div>
                     </div>
-                </div>
-            </button>
-        </div>
-    );
-    
+                </button>
+            </div>
+        )
+    };
+
     const renderMobileCapSection = (options) => (
         <section className="flex justify-center items-center gap-x-[0.938rem] py-[1.625rem] lg:hidden">
             <h1 className="font-outfit text-links font-medium">Personas:</h1>
@@ -101,7 +109,7 @@ export function Hospedaje() {
             ))}
         </section>
     );
-    
+
     const renderDesktopCapSection = (options) => (
         <section className="justify-center items-center gap-x-[0.938rem] py-[1.625rem] hidden lg:flex">
             {options.map(({ text, servicio }) => (
@@ -109,7 +117,7 @@ export function Hospedaje() {
             ))}
         </section>
     );
-    
+
     const renderServiceSection = (serviceIcon) => (
         <section className="lg:hidden">
             <h1 className="font-outfit text-links font-medium">Descripción:</h1>
@@ -121,13 +129,13 @@ export function Hospedaje() {
             </div>
         </section>
     );
-    
+
     const renderReserveButton = () => (
         <div className="bg-blue-1 rounded-xl py-[0.626rem] flex justify-center mb-[1.625rem] lg:hidden">
             <a className="text-white text-ovo text-base" href="https://wa.me/message/ZGYH7OW6HZAEN1">Reservar</a>
         </div>
     );
-    
+
     const renderWarning = (message) => (
         <p className="font-links text-gray-1 font-outfit text-center pb-14">{message}</p>
     );
@@ -139,39 +147,39 @@ export function Hospedaje() {
             </div>
 
             {/* Sección para las cabinas */}
-            
-        {activeTab === 'Cabinas' && (
-            <div className="md:px-[3.125rem] px-[1rem]">
-                {renderOpcionSection()}
-                {renderButtonSection('right-[-2rem]')}
-                {renderMobileCapSection(['3-4', '5-6', '5-6*', '8-9'])}
-                {renderDesktopCapSection([
-                    { text: '3-4 Personas', servicio: 'Abanico' },
-                    { text: '5-6 Personas', servicio: 'Abanico' },
-                    { text: '5-6* Personas', servicio: 'A/C' },
-                    { text: '8-9 Personas', servicio: 'Abanico' },
-                ])}
-                {renderServiceSection(cabinaEscogida.servicio === 'Abanico' ? fanBlue : acBlue)}
-                {renderReserveButton()}
-                {renderWarning('Se permite máximo una persona adicional en cada cabina (esto por motivos de seguridad), la cual debe cancelar un monto de ₡5.000 cada noche y debe traer su propio colchón.')}
-            </div>
-        )}
-        {activeTab === 'Habitaciones' && (
-            <div className="md:px-[3.125rem] px-[1rem]">
-                {renderOpcionSection()}
-                {renderButtonSection('left-[-2rem]')}
-                {renderMobileCapSection(['1', '2', '3', '4'])}
-                {renderDesktopCapSection([
-                    { text: '1 Persona', servicio: 'A/C' },
-                    { text: '2 Personas', servicio: 'A/C' },
-                    { text: '3 Personas', servicio: 'A/C' },
-                    { text: '4 Personas', servicio: 'A/C' },
-                ])}
-                {renderServiceSection(acBlue)}
-                {renderReserveButton()}
-                {renderWarning('Es prohibido cocinar dentro de la habitación, ya que el espacio no es apto para realizar esta actividad.')}
-            </div>
-        )}
+
+            {activeTab === 'Cabinas' && (
+                <div className="md:px-[3.125rem] px-[1rem]">
+                    {renderOpcionSection()}
+                    {renderButtonSection('right-[-2rem]')}
+                    {renderMobileCapSection(['3-4', '5-6', '5-6*', '8-9'])}
+                    {renderDesktopCapSection([
+                        { text: '3-4 Personas', servicio: 'Abanico' },
+                        { text: '5-6 Personas', servicio: 'Abanico' },
+                        { text: '5-6* Personas', servicio: 'A/C' },
+                        { text: '8-9 Personas', servicio: 'Abanico' },
+                    ])}
+                    {renderServiceSection(cabinaEscogida.servicio === 'Abanico' ? fanBlue : acBlue)}
+                    {renderReserveButton()}
+                    {renderWarning('Se permite máximo una persona adicional en cada cabina (esto por motivos de seguridad), la cual debe cancelar un monto de ₡5.000 cada noche y debe traer su propio colchón.')}
+                </div>
+            )}
+            {activeTab === 'Habitaciones' && (
+                <div className="md:px-[3.125rem] px-[1rem]">
+                    {renderOpcionSection()}
+                    {renderButtonSection('left-[-2rem]')}
+                    {renderMobileCapSection(['1', '2', '3', '4'])}
+                    {renderDesktopCapSection([
+                        { text: '1 Persona', servicio: 'A/C' },
+                        { text: '2 Personas', servicio: 'A/C' },
+                        { text: '3 Personas', servicio: 'A/C' },
+                        { text: '4 Personas', servicio: 'A/C' },
+                    ])}
+                    {renderServiceSection(acBlue)}
+                    {renderReserveButton()}
+                    {renderWarning('Es prohibido cocinar dentro de la habitación, ya que el espacio no es apto para realizar esta actividad.')}
+                </div>
+            )}
 
             <Footer />
         </div>
